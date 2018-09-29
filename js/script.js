c8962f8092215d0ae3ff74d72e4f04569b9e28c2
Nicolas <43158653+NicolasOlenev@users.noreply.github.com>
2018-09-29 18:56:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 'use strict'
-window.onload = function() {    // Загружать скрипт только после полной загрузки DOM
-  
-    
-var wrapper = document.querySelector('.wrapper');
-var table = document.createElement('table');
-var ltrs = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '0'];
-var nmbrs = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+window.onload = function() {    // Загружать скрипт только после полной загрузки DOM  
+var wrapper = document.querySelector('.wrapper'),
+    table = document.createElement('table'),
+    ltrs = ['0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '0'],
+    nmbrs = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
 wrapper.appendChild(table);
-
-/* создаем шахматную доску; раскрашиваем клетки; отмечаем боковые поля для цифр и букв */
+{   // создаем шахматную доску; раскрашиваем клетки; отмечаем боковые поля для цифр и букв (начало)
 for (var i = 0; i < 10; i++) {
     var row = document.createElement('tr');
     for (var j = 0; j < 10; j++) {
@@ -24,11 +21,10 @@ for (var i = 0; i < 10; i++) {
     }
     table.appendChild(row); 
 }
-
-/* Расставляем буквы и цифры на боковых полях */
-{
-    var border = document.querySelectorAll('.border');
-    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+}   // создаем шахматную доску; раскрашиваем клетки; отмечаем боковые поля для цифр и букв (конец)
+{   // Расставляем буквы и цифры на боковых полях (начало)
+    var border = document.querySelectorAll('.border'),
+        letters = ltrs.splice(1, 8);
     for (i = 1, j = 0; i < 9; i++, j++) {
         border[i].innerText = letters[j];
         border[i].classList.add('textDown');
@@ -38,22 +34,10 @@ for (var i = 0; i < 10; i++) {
         border[i++].innerText = border[i].innerText = j;
         border[i].classList.add('textDown');
     }
-}
-
-/* попытка подключить SVG */
-/* 
-var mySVG = document.getElementById("object");
-var svgDocument = mySVG.addEventListener('load',function() {
-var svgDoc = this.contentDocument;
-var svgElement = svgDoc.getElementById("text1061");
-secondRow[35].inputMode = svgElement;  
-}, false);
-*/
-
-var playground = document.querySelectorAll('td');
-var color;
-    
-/* расставляем фигуры */
+}   // Расставляем буквы и цифры на боковых полях (конец)
+{   // расставляем фигуры (начало)
+var playground = document.querySelectorAll('td'), 
+    color;
 for (i = 11; i < 89; i++) {
     var img = document.createElement('img');
     img.height = '45';
@@ -78,26 +62,34 @@ for (i = 11; i < 89; i++) {
     if (playground[i].classList[1] != 'border' && (i < 29 || i > 70))
         playground[i].appendChild(img);
 }
-
-/* учим фигуры двигаться */
-{
-var selectedTd; // Используется для подсветки клетки под фигурой в функции "highlight"
-var inHand = null; // Используется для хранения выбранной для хода фигуры
+}   // расставляем фигуры (конец)
+{   // Учим фигуры двигаться (начало)
+var selectedTd, // Используется для подсветки клетки под фигурой в функции "highlight"
+    inHand = null; // Используется для хранения выбранной для хода фигуры
+    
 function gamerIs(src) {     // Функция определяет, какого цвета фигура в руке игрока
-    for (var i = 0; i <= src.length; i++) {
+    for (i = 0; i <= src.length; i++) {
         if (src.charAt(i) == 'W' && src.charAt(++i) == 'h')
             return 'белых';  
         else if (src.charAt(i) == 'B' && src.charAt(++i) == 'l')
             return 'черных';
     }
 }   // Функция определяет, какого цвета фигура в руке игрока
+    
 function highlight(node) {      // Функция для подсветки клетки под выбранной фигурой и клетки, на которую фигуру переместили
-    if (selectedTd) {
+    if (selectedTd)
         selectedTd.classList.remove('highlight');  
-    }
     selectedTd = node;
     selectedTd.classList.add('highlight');
 }   // Функция для подсветки клетки под выбранной фигурой и клетки, на которую фигуру переместили
+       
+function entry(str) { // Функция записи хода в таблицу ходов
+    if (str == 'черных')
+        space = ': ';   // Это нужно, чтобы выровнять по длине строчки ходов, т.к. "черных" длинее "белых",
+    else space = ':  '; // к "белых" нужно прибавлять дополнительный пробел
+        document.querySelector('textarea').innerHTML += 'Ход ' + str + space + strokeWay + '\u000A'; // Сделать запись
+}    // Функция записи хода в таблицу ходов 
+    
 var stroke = 1, // Счетчик, определяющий ход игрока (нечетный - белые; четный - черные)
     player = ['белых', 'черных'],
     playerReverse = ['черных', 'белых'],
@@ -105,58 +97,53 @@ var stroke = 1, // Счетчик, определяющий ход игрока 
     strokePlayer,
     strokeWay,
     space;
-//console.log('Ход белых:');
+
 table.onclick = function(event) {   // Функция обработчика событий "onclick", передвигает фигурки
     var target = event.target;
     while (target != table) {
         if (target.classList.contains('white') || target.classList.contains('black')) {
-            if (stroke % 2 != 0) {
+            if (stroke % 2 != 0) {  // Ход белых
                 strokePlayer = player[0];
                 strokePlayerReverse = playerReverse[0];
-                //console.log('Ход белых:');
-            } else {
+            } else {                // Ход черных
                 strokePlayer = player[1];
                 strokePlayerReverse = playerReverse[1];
-                //console.log('Ход черных:');
             }
-                
+                /* Если в руке нет фигуры, и в клетке есть фигура, и цвет фигуры соответствует цвету,
+                   который сейчас ходит, то взять в руку эту фигуру, клетку подсветить, записать
+                   номер этой клетки в переменную "strokeWay", инкрементировать счетчик определия следующего хода соперника */
             if (inHand == null && target.firstChild && gamerIs(target.firstChild.getAttribute('src')) == strokePlayer) {
                 inHand = target.firstChild;
                 highlight(target);
-                //console.log('В руке фигура', gamerIs(inHand.getAttribute('src')));
                 strokeWay = target.classList[0];
-                //console.log(strokeWay);
                 stroke++;   
             }
+                /* Если в руке есть фигура и клетка, в которую хотят переместить фигуру ялвяется пустой,
+                   то подсветить эту клетку (предыдущая подсвеченная потухнет), поместить в клетку фигуру
+                   и удалить ее из руки, дописать в переменную "strokeWay" номер соответствующей клетки,
+                   вписать ход игрока в таблицу ходов */
             if (inHand != null && !target.firstChild) {
                 highlight(target);
                 target.appendChild(inHand);
                 inHand = null;
-                //console.log('В руке фигурры нет');
                 strokeWay += ('-' + target.classList[0]);
-                //console.log('Ход ' + strokePlayerReverse + ': ' + strokeWay);
-                if (strokePlayerReverse == 'черных')
-                    space = ': ';
-                else space = ':  ';
-                document.querySelector('textarea').innerHTML += 'Ход ' + strokePlayerReverse + space + strokeWay + '\u000A';
+                entry(strokePlayerReverse);
             }
+                /* Если в руке есть фигура и ее цвет противоположен цвету фигуры в клетке, на которую хотят переместить
+                   фигуру из руки, то удалить фигуру из этой клетки и поместить в клетку фигуру из руки, дописать в
+                   переменную "stokeWay" номер соотвутствующей клетки, вписать ход игрока в таблицу ходов */
             if (inHand != null && gamerIs(target.firstChild.getAttribute('src')) != gamerIs(inHand.getAttribute('src'))) {
                 target.innerHTML = '';
                 target.appendChild(inHand);
                 strokeWay += ('-' + target.classList[0]);
-                //console.log('Ход ' + strokePlayerReverse + ': ' + strokeWay);
-                document.querySelector('textarea').innerHTML += 'Ход ' + strokePlayerReverse + space + strokeWay + '\u000A';
-                //console.log('фигура съела фигуру соперника');
+                entry(strokePlayerReverse);
                 highlight(target);
                 inHand = null;
             }
-            
             return;
         }
         target = target.parentNode;
     }
 }   // Функция обработчика событий "onclick", передвигает фигурки
-}
-  
-    
+}   // Учим фигуры двигаться (конец) 
 }   // Загружать скрипт только после полной загрузки DOM
